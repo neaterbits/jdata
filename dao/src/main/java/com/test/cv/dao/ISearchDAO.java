e442df5866436f51e2fc9549f0a94db84bff82a6
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2018-02-12 10:16:26 -0300

    Remove freetext parameter for now since requires native SQL queries, no freetext support in JPA

--- dao/src/main/java/com/test/cv/dao/ISearchDAO.java
+++ dao/src/main/java/com/test/cv/dao/ISearchDAO.java
@@ -1,6 +1,7 @@
 package com.test.cv.dao;
 
 import com.test.cv.dao.criteria.SearchCriteria;
+import com.test.cv.model.Item;
 
 /**
  * DAO for generic search over items in storage, returns paged results
@@ -11,11 +12,14 @@ public interface ISearchDAO {
 	/**
 	 * Search for items using a freetext and a number for criteria to match
 	 * 
-	 * @param freeText
+	 * @param type item type
+	 * @param freeText freetext to search for
 	 * @param criteria list of criteria
 	 * @return a cursor for sshowing results, may be a long list of items
+	 * 
+	 * @todo add freetext
 	 */
-	ISearchCursor search(String freeText, SearchCriteria ... criteria);
+	ISearchCursor search(Class<? extends Item> type /*, String freeText */, SearchCriteria ... criteria);
 
 }
 
