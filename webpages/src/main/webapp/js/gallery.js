7b803d4212ba0d777dc03a6ac1f85a3a83f87d1f
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2018-03-24 17:42:18 -0400

    Moved gallery debug functions into separate base class

--- webpages/src/main/webapp/js/gallery.js
+++ webpages/src/main/webapp/js/gallery.js
@@ -817,58 +817,8 @@ function Gallery(divId, config, galleryModel, galleryView) {
 	this._getInnerElement = function() {
 		return this.innerDiv;
 	}
-
-	this.enter = function(level, functionName, args) {
-		var argsString;
-		
-		if (typeof args === 'undefined') {
-			argsString = '';
-		}
-		else {
-			argsString = '';
-		
-			if (args.length % 2 != 0) {
-				throw 'Number of arguments to ' + functionName + ' not an even number: ' + args.length;
-			}
-			
-			for (var i = 0; i < args.length; i += 2) {
-				if (i > 0) {
-					argsString += ', ';
-				}
-
-				argsString += args[i] + '=' + args[i + 1];
-			}
-		}
-		
-		
-		this._log(level, 'ENTER ' + functionName + '(' + argsString + ')');
-	};
-	
-	
-	this.exit = function(level, functionName, returnValue) {
-		
-		var returnString = typeof returnValue === 'undefined'
-			? ""
-			: " = " + returnValue;
-		
-		this._log(level, 'EXIT ' + functionName + '()' + returnString);
-	};
-
-	this.log = function(level, text) {
-		this._log(level + 1, text);
-	};
-
-	this._log = function(level, text) {
-		console.log(this.indent(level) + text);
-	};
-	
-	this.indent = function(level) {
-		var s = "";
-
-		for (var i = 0; i < level; ++ i) {
-			s += "  ";
-		}
-		
-		return s;
-	}
 }
+
+
+Gallery.prototype = Object.create(GalleryBase.prototype);
+
---
+++ webpages/src/main/webapp/js/gallery_base.js
@@ -0,0 +1,61 @@
+/**
+ * Base class functions for all gallery based objects
+ *  
+ */
+
+function GalleryBase() {
+	
+}
+
+GalleryBase.prototype.enter = function(level, functionName, args) {
+	var argsString;
+	
+	if (typeof args === 'undefined') {
+		argsString = '';
+	}
+	else {
+		argsString = '';
+	
+		if (args.length % 2 != 0) {
+			throw 'Number of arguments to ' + functionName + ' not an even number: ' + args.length;
+		}
+		
+		for (var i = 0; i < args.length; i += 2) {
+			if (i > 0) {
+				argsString += ', ';
+			}
+
+			argsString += args[i] + '=' + args[i + 1];
+		}
+	}
+	
+	
+	this._log(level, 'ENTER ' + functionName + '(' + argsString + ')');
+};
+
+GalleryBase.prototype.exit = function(level, functionName, returnValue) {
+	
+	var returnString = typeof returnValue === 'undefined'
+		? ""
+		: " = " + returnValue;
+	
+	this._log(level, 'EXIT ' + functionName + '()' + returnString);
+};
+
+GalleryBase.prototype.log = function(level, text) {
+	this._log(level + 1, text);
+};
+
+GalleryBase.prototype._log = function(level, text) {
+	console.log(this.indent(level) + text);
+};
+
+GalleryBase.prototype.indent = function(level) {
+	var s = "";
+
+	for (var i = 0; i < level; ++ i) {
+		s += "  ";
+	}
+	
+	return s;
+}
