04b6ec26a08710b01142b24f3b5f968ee799145e
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2019-05-21 17:52:52 +0200

    Remove unused import

--- model-items-base/src/main/java/com/test/salesportal/model/items/photo/ItemPhoto.java
+++ model-items-base/src/main/java/com/test/salesportal/model/items/photo/ItemPhoto.java
@@ -10,7 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 import com.test.salesportal.model.items.Item;
 
